510838ef594fef1dd9f6b2b4c542ee9ff0fbd1bf
Merrick101 <yt7i0aoi@students.codeinstitute.net>
2025-02-18 19:27:26 +0000

    Remove user_messages app and replace with inbox

--- free_linked/settings.py
+++ free_linked/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'users',
     'jobs',
     'proposals',
-    'user_messages',
+    'inbox',
     'reviews',
 ]
 
@@ -121,7 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
