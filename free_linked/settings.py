63f84d8b473b170a4daf250f6a576bd4355e3d1d
Merrick101 <yt7i0aoi@students.codeinstitute.net>
2025-02-18 18:36:15 +0000

    Fix issues in settings causing unsuccessful deployment

--- free_linked/settings.py
+++ free_linked/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e56#28x@m=iat05(0@8brx2^4pp(w3326@$w%q1k!iw^%9x2p%'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [['.herokuapp.com']]
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -85,9 +85,6 @@ WSGI_APPLICATION = 'free_linked.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
-
-
-SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # Password validation
